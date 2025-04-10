ae60eaf335cc7ad18e8e54db81d24a5624a516e4
Maimona <maimonamustaq@gmail.com>
2025-04-10 19:06:44 +0500

    add modulus feature

--- calculator.cpp
+++ calculator.cpp
@@ -7,6 +7,8 @@ int main() {
     std::cout << "Sum: " << a + b << std::endl;
     std::cout << "Difference: " << a - b << std::endl;
     std::cout << "Product: " << a * b << std::endl;
+    std::cout << "Remainder: " << a % b << std::endl;
+
     if (b != 0) { // Added a check to avoid division by zero.
         std::cout << "Quotient: " << static_cast<double>(a) / b << std::endl;
     } else {
